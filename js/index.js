3288028d6518517dc16ed9de9cc5159c0d6d9491
Jakub Figlak <jakub.figlak@gmail.com>
2020-03-03 18:57:45 +0100

    Add display success function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,17 @@
 const signupInputs = document.querySelectorAll('.signup__input');
 
+function clearPrevState(input) {
+  if (input.classList.contains('signup__input--invalid')) {
+    console.log('input invalid change');
+    const field = input.closest('.signup__field');
+    const errorMsg = field.querySelector('.signup__error');
+    errorMsg.classList.remove('signup__error--active');
+    input.classList.remove('signup__input--invalid');
+  } else if (input.classList.contains('signup__input--valid')) {
+    input.classList.remove('signup__input--valid');
+  }
+}
+
 function displayError(input, message) {
   const field = input.closest('.signup__field');
   const errorMsg = field.querySelector('.signup__error');
@@ -8,23 +20,33 @@ function displayError(input, message) {
   input.classList.add('signup__input--invalid');
 }
 
-function validateInputs() {
-  const { valueMissing, typeMismatch, tooShort } = this.validity;
+function displaySuccess(input) {
+  input.classList.add('signup__input--valid');
+}
 
-  if (valueMissing) {
-    displayError(this, 'This field is required');
-  }
-  if (typeMismatch) {
-    displayError(this, 'Please enter a valid e-mail');
-  }
-  if (tooShort) {
-    displayError(
-      this,
-      `${this.name} should be at least ${this.minLength} chars long`
-    );
+function validateInput() {
+  clearPrevState(this);
+  const { valid, valueMissing, typeMismatch, tooShort } = this.validity;
+
+  if (valid) {
+    displaySuccess(this);
+  } else {
+    if (valueMissing) {
+      displayError(this, 'This field is required');
+    }
+    if (typeMismatch) {
+      displayError(this, 'Please enter a valid e-mail');
+    }
+    if (tooShort) {
+      displayError(
+        this,
+        `${this.name} should be at least ${this.minLength} chars long`
+      );
+    }
   }
 }
 
-signupInputs.forEach(input =>
-  input.addEventListener('invalid', validateInputs)
-);
+signupInputs.forEach(input => {
+  input.addEventListener('invalid', validateInput);
+  input.addEventListener('blur', validateInput);
+});
